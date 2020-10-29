c7956b3dc73c64335250d0f377729459fca9f65a
Dan Hoban <dan@doppio3.digital>
2020-10-30 09:54:32 +1000

    Base Pluginfactory.io Files

---
+++ src/translations/en/xero.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Xero plugin for Craft CMS 3.x
+ *
+ * A Craft Commerce Integration for Xero
+ *
+ * @link      https://doppio3.digital
+ * @copyright Copyright (c) 2020 Dan Hoban
+ */
+
+/**
+ * Xero en Translation
+ *
+ * Returns an array with the string to be translated (as passed to `Craft::t('xero', '...')`) as
+ * the key, and the translation as the value.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
+ *
+ * @author    Dan Hoban
+ * @package   Xero
+ * @since     1.0.0
+ */
+return [
+    'Xero plugin loaded' => 'Xero plugin loaded',
+];
